f6658c4ce6c776f88a427e517f1f4c4ce87c3193
Douglas Leonel Guillen Soriano <douglas.guillen@outlook.es>
2019-09-30 16:56:12 -0600

    Search Component

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useContext } from 'react'
+import { RoomContext } from '../context'
+import Title from '../components/Title'
 
-export default function RoomsFilter() {
+// Get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomsFilter({ rooms }) {
+    const context = useContext(RoomContext)
+    const {
+        handleChanged,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
+
+    let types = getUnique(rooms, 'type');
+    types = ['all', ...types];
+    types = types.map((item, index) => {
+        return <option index={index} value={item}>{item}</option>
+    })
+
     return (
-        <div>
-            Hello From Rooms Filter
-        </div>
+        <section className="filter-container">
+            <Title title="search romms" />
+            <form className="filter-form">
+                {/*select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id="type" value={type} className="form-control" onChange={handleChanged}>
+                        {types}
+                    </select>
+                </div>
+                {/* end selete type */}
+            </form>
+        </section>
     )
 }
