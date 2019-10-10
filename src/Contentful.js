5d5bbdc8a30a758a2870907e0c5a03ca87b755fc
Douglas Leonel Guillen Soriano <douglas.guillen@outlook.es>
2019-10-10 15:28:28 -0600

    Implement Contentful

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import { createClient } from 'contentful'
+
+export default createClient({
+    space: "j1t01yknb73s",
+    accessToken: "vr7eExwjjIKitu2Ib_i_6zjjrVDZEKSc6ra56dYHTxw"
+})
--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import items from './data'
+// import items from './data'
+import Client from './Contentful'
 
 const RoomContext = React.createContext();
 
@@ -21,21 +22,34 @@ export default class RoomProvider extends Component {
     };
 
     // getData
-    componentDidMount() {
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: 'beachResortRoom',
+                order: 'fields.price'
+            });
 
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize,
-        })
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    componentDidMount() {
+        this.getData();
     }
 
     formatData(items) {
