c469bf44ed3200239f5d67eff280f86316ec28ea
Douglas Leonel Guillen Soriano <douglas.guillen@outlook.es>
2019-10-11 10:52:51 -0600

    Before deployment

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful'
 
 export default createClient({
-    space: "j1t01yknb73s",
-    accessToken: "vr7eExwjjIKitu2Ib_i_6zjjrVDZEKSc6ra56dYHTxw"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
