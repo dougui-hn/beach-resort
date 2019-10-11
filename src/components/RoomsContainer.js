07dc8ce0206e43211194f94b509d2cbd168a8ba5
Douglas Leonel Guillen Soriano <douglas.guillen@outlook.es>
2019-10-11 11:43:44 -0600

    Last improment

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
 import { withRoomConsumer } from '../context';
+import Loading from './Loading';
 
 function RoomsContainer({ context }) {
     const { loading, sortedRooms, rooms } = context
 
-    // if (loading){
-    //     return <Loading />
-    // }
+    if (loading) {
+        return <Loading />
+    }
 
     return (
         <>
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
@@ -9,7 +9,6 @@ import StyledHero from '../components/StyledHero'
 export default class SingleRoom extends Component {
     constructor(props) {
         super(props)
-        // console.log(this.props)
         this.state = {
             slug: this.props.match.params.slug, defaultBcg
         }
